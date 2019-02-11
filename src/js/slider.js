ea258929dfba2f0a49b6878fd0a55b8baabc1882
zagagylina <zagaggylina@gmail.com>
2019-02-11 20:39:21 +0200

    first commit

---
+++ src/js/slider.js
@@ -0,0 +1,55 @@
+;
+var slideIndex = 1;
+showSlides(slideIndex);
+
+// function plusSlides(n) {
+//   showSlides(slideIndex += n);
+// }
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+  fillModal(slideIndex); // Fill modal window of current slide 
+}
+
+function showSlides(n) {
+  var i;
+  var slides = document.getElementsByClassName("slider__item");
+  var dots = document.getElementsByClassName("slider__dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+      slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+      dots[i].className = dots[i].className.replace(" slider__dot--active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " slider__dot--active";
+}
+
+
+
+// Get collection of content from all slides
+var slideTexts = document.getElementsByClassName("slider__text");
+
+var modalText = document.getElementById("content01");
+
+fillModal(slideIndex);
+
+function fillModal(n){
+  modalText.innerHTML = slideTexts[n-1].innerHTML;
+}
+
+
+var modal = document.getElementById("id01");
+function showModal(){
+  modal.style.display = 'block';
+}
+
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
